f0cf276f6f79f9af0df741c1cb9b2cb9117a8b37
august269 <akirui95@gmail.com>
2023-03-19 07:46:21 +0300

    use md5 hashing for password security

--- users.js
+++ users.js
@@ -1,7 +1,6 @@
 //imports
 require('dotenv').config(); //to access environ variables
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 //constants
 const dbName = "usersdb";
@@ -11,9 +10,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-//add encrypt plugin to schema
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 //db model
 const User = mongoose.model("user", userSchema);
